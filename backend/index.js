683730cd21df2b329ca33ceae412da872f2f145b
LavanyaSingla <60093348+LavanyaSingla@users.noreply.github.com>
2024-04-07 20:43:08 +0530

    Create backend folder and index.js file

---
+++ backend/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const mongoDB = require("./db");
+const createuser = require("./Routes/CreateUser");
+
+// CORS middleware
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+// Connect to MongoDB
+mongoDB();
+
+// Hello World route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Mount the createUser route
+app.use('/api', createuser);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
