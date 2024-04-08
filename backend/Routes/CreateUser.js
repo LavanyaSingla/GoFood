27eb11d8a11594841bacd0d51b97f02afafb23da
LavanyaSingla <60093348+LavanyaSingla@users.noreply.github.com>
2024-04-08 20:15:52 +0530

    Add hashing and authorization in Signup & Login

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 router.post("/createuser", [
     body('email', 'Invalid email').isEmail(),
@@ -10,15 +11,17 @@ router.post("/createuser", [
 ],
     async (req, res) => {
         const errors = validationResult(req);
-        if (errors) {
+        if (!errors.isEmpty()) {
             return res.status(400).json({ success: false, error: errors.array(), errrorr: errors });
         }
         try {
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
             const newUser = await User.create({
                 name: req.body.name,
                 email: req.body.email,
                 location: req.body.location,
-                password: req.body.password
+                password: secPass
             });
             res.json({ success: true, user: newUser });
         } catch (error) {
@@ -40,7 +43,8 @@ router.post("/loginuser", [
         try {
             const user = await User.findOne({ email });
             if (!user) return res.status(400).json({ success: false, error: "This user does not exist" });
-            if (user.password !== req.body.password) return res.status(400).json({ success: false, error: "Invalid password" });
+            const pwdCompare = bcrypt.compare(req.body.password, user.password)
+            if (!pwdCompare) return res.status(400).json({ success: false, error: "Invalid password" });
 
             res.json({ success: true });
         } catch (error) {
